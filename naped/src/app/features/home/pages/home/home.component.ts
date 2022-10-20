677cf07e3686c06a1935dcb71e7eff0eaaa52f37
Eduardo Marra <eduardomarra@gmail.com>
2022-10-20 19:45:14 -0300

    adicionado pop-up na tela de login

--- naped/src/app/features/home/pages/home/home.component.ts
+++ naped/src/app/features/home/pages/home/home.component.ts
@@ -9,10 +9,7 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    setTimeout(() =>{
-      alert('Este projeto foi desenvolvido para aplicar conhecimentos em Angular. Apenas para fins didáticos.')
-      alert('Para acessar configurações de ADM, logar com adm@gmail.com (123456)')
-    },3000)
+
   }
 
 }
--- naped/src/app/features/login/pages/login/login.component.ts
+++ naped/src/app/features/login/pages/login/login.component.ts
@@ -18,6 +18,10 @@ export class LoginComponent implements OnInit {
     ) { }
 
   ngOnInit(): void {
+    setTimeout(() =>{
+      alert('Este projeto foi desenvolvido para aplicar conhecimentos em Angular. Apenas para fins didáticos.')
+      alert('Para acessar configurações de ADM, logar com adm@gmail.com (123456)')
+    },3000)
   }
 
   authenticateByUser() {
